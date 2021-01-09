8d0ef6fbb126e63c72bc0e7b96771657a07f4297
Ulysse3311 <ulyssebbbb@gmail.com>
2021-01-10 00:27:46 +0100

    Api is now functional Added new test request : vector of 0, first line from dataset, 2 raw emails

--- api.py
+++ api.py
@@ -8,20 +8,86 @@ from flask import Flask, request, redirect, url_for, flash, jsonify
 import numpy as np
 import pickle as p
 import json
+import re
+import pandas as pd
+
+"""
+Request to the api must have the following format :
+either :
+data = [1,[xi]*57]
+with every xi corresponding to a columns
+or :
+data = [0,"raw email text"]
+
+data[0] is a flag to know if the data is a row email or a vector 
+"""
 
 
 app = Flask(__name__)
 
-
 @app.route('/api', methods=['POST'])
 def makecalc():
     data = request.get_json()
-    print(data)
-    prediction = [0,0,0]
+    print("incoming data : \n",data)
+    line=0
+    if data[0]==1 and len(data[1][0])==57:
+        print("Request type : vector")
+        line = data[1]
+    elif data[0]==0:
+        print("Request type :raw email")
+        line=[conv(d,col_names) for d in data[1]]
 
-    return jsonify(prediction)
+    if line!=0:
+        prediction = np.array2string(model.predict(line))
+        print("pred : \n",prediction)
+        return jsonify(prediction)
+    else :
+        print("non compatible data")
+
+def conv(mail,columns_names,prt=False):
+    vect=[]
+    nb_word=len(re.findall(r'\w+',mail))
+    
+    #word/char freq
+    mail_low=mail.lower()
+    for col in columns_names[:-4]: # the last tree dont look for word/char freq
+        ref=col.split("_")[2] # the columns names a in the format : word/char_freq_ref
+        if ref in ["(","["]:
+            ref="\\"+ref
+        match_count=len(re.findall(f"({ref})",mail_low))
+        if prt:
+            print(ref,match_count)
+        vect.append(100*match_count/nb_word)
+    
+    # last 3 variables
+    # every sentence in capital letters
+    capital =re.findall("[A-Z, ,\d,\,]{2,}",mail)
+    longest=0
+    sum_cap=0
+    if len(capital)!=0:
+        for match in capital:
+            sum_cap+=len(match.replace(" ","")) # removing " " to get the total number of capital letter
+            size=len(match)
+            if size>longest:
+                longest=size
+    else :
+        capital=[1]
+    vect.append(sum_cap/len(capital)) # average length of uninterrupted sequences of capital letters
+    vect.append(longest) # length of longest uninterrupted sequence of capital letters
+    vect.append(sum_cap) # total number of capital letters in the e-mail
+    
+    return vect
+
+def get_col_names():
+    columns=[]
+    with open("columns.txt","r") as doc:
+        lines=doc.readlines()
+        for line in lines :
+            columns.append(line.split(":")[0])
+    return columns
 
 if __name__ == '__main__':
-    #modelfile = 'models/final_prediction.pickle'
-    #model = p.load(open(modelfile, 'rb'))
+    modelfile = r'finalized_model.pickle'
+    model = p.load(open(modelfile, 'rb'))
+    col_names=get_col_names()
     app.run()
--- request.py
+++ request.py
@@ -8,11 +8,39 @@ Created on Fri Jan  8 19:52:54 2021
 import requests
 import json
 
-url = 'http://127.0.0.1:5000/api'
 
-data = [1,[14.34, 1.68, 2.7, 25.0, 98.0, 2.8, 1.31, 0.53, 2.7, 13.0, 0.57, 1.96, 660.0]]
-j_data = json.dumps(data)
-print(j_data)
-headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-r = requests.post(url, data=j_data, headers=headers)
-print(r, r.text)
+
+def send_request(data):
+    j_data = json.dumps(data)
+    print(j_data)
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    r = requests.post(url, data=j_data, headers=headers)
+    print(r, r.text)
+
+def send_zeros(): #send a vector 0
+    line = [[0] * 57]
+    data = [1, line]
+    send_request(data)
+
+def send_first_mail(): #send the first line from the dataset, it should be labeled as a spam (1)
+    line = [[  0.,   1.,   1.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,
+         1.,   0.,   0.,   0.,   0.,   0.,   1.,   2.,   0.,   1.,   0.,
+         0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,
+         0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,   0.,
+         0.,   0.,   0.,   0.,   0.,   0.,   0.,   1.,   0.,   0.,   4.,
+        61., 278.]]
+    data=[1,line]
+    send_request(data)
+
+def send_raw_emails():
+    email1 = "You have won 1 000 000 dollars, please send your address !"
+    email2= "Hello George, How are you ?"
+    line =[email1,email2]
+    data=[0,line]
+    send_request(data)
+
+if __name__=="__main__":
+    url = 'http://127.0.0.1:5000/api'
+    send_zeros()
+    send_first_mail()
+    send_raw_emails()
